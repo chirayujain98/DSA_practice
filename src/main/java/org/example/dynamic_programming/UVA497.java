1259b322373e77ad59cda0b3f2796538ce23fb4e
chirayujain98 <chirayu.jain98@gmail.com>
2024-12-31 15:32:56 +0530

    Optimising construction of array code

--- src/main/java/org/example/dynamic_programming/UVA497.java
+++ src/main/java/org/example/dynamic_programming/UVA497.java
@@ -6,49 +6,46 @@ import java.util.List;
 import java.util.Scanner;
 
 public class UVA497 {
-    //TODO: While submitting getting TLE. Need to optimize.
-    public static void LIS(List<Integer> list) {
-        int n = list.size();
+    public static void LIS(List<Integer> altitudes, int t) {
+        int n = altitudes.size();
         int[] dp = new int[n];
+        int[] prev = new int[n];  // Store previous missile index for backtracking
+        
+        // Initialize
         for(int i = 0; i < n; i++) {
             dp[i] = 1;
+            prev[i] = -1;
         }
-        int maxLength = 0;
-        int index = 0;
+        
+        // Find longest sequence
+        int maxLength = 1;
+        int endIndex = 0;
         for(int i = 1; i < n; i++) {
             for(int j = 0; j < i; j++) {
-                if(list.get(i) > list.get(j)) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                if(altitudes.get(i) > altitudes.get(j) && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
                     if(dp[i] > maxLength) {
                         maxLength = dp[i];
-                        index = i;
+                        endIndex = i;
                     }
                 }
             }
         }
-        System.out.println("Max hits: "+ maxLength);
-        int r = index;
-        int element = list.get(index);
-        List<Integer> result = new ArrayList<>();
-        result.add(element);
         
-        maxLength--;
-        while(maxLength!=0) {
-            for(int j = 0; j < r; j++) {
-                if(list.get(j) < element && dp[j] == maxLength) {
-                    element = list.get(j);
-                    result.add(element);
-                    r=j;
-                    maxLength--;
-                    break;
-                }
-            }
+        // Reconstruct sequence
+        List<Integer> sequence = new ArrayList<>();
+        for(int i = endIndex; i != -1; i = prev[i]) {
+            sequence.add(altitudes.get(i));
         }
-        Collections.reverse(result);
-        for(int i = 0; i < result.size(); i++) {
-            System.out.println(result.get(i));
+        Collections.reverse(sequence);
+        
+        // Output results
+        System.out.println("Max hits: " + maxLength);
+        for(int altitude : sequence) {
+            System.out.println(altitude);
         }
-        System.out.println();
+        if(t != 0) System.out.println();
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -60,7 +57,7 @@ public class UVA497 {
             while(sc.hasNextLine() && !(line = sc.nextLine()).isEmpty()) {
                 list.add(Integer.parseInt(line));
             }
-            LIS(list);
+            LIS(list, t);
         }
         sc.close();
     }
